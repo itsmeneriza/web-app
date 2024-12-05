9bf97a5b984dea8fe14f039f7b454382ce3b72ef
Neriza Saldo <saldoneriza19@gmail.com>
2024-12-06 00:14:34 +0800

    edited server.js for render

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const characterRoutes = require('./routes/characterRoutes');
 const PORT = process.env.PORT || 3000;
 
@@ -9,8 +10,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve static files (Front-end)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Character routes
 app.use('/lionking', characterRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Start the server
 app.listen(PORT, () => {
